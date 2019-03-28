e22064346924afd54af405332bb88804fe854aa3
Matt Neville <mattneville17@gmail.com>
2019-03-28 17:06:48 +0000

    Update processOpenProt.R

--- processOpenProt.R
+++ processOpenProt.R
@@ -13,17 +13,25 @@ library(Biostrings)
 
 #Read in BED, FASTA, and TSV files
 bedFile <- read_tsv("openProtAllPredicted_38.bed", col_names = F, col_types = "ciiciciicicc")
-tsvFile <- read.delim("openProtAllPredicted.tsv", header = T, skip = 1)
-dnaFasta <- readDNAStringSet("openProtAllPredicted_38.dna.fasta") 
+tsvFile <- read_tsv("openProtAllPredicted.tsv", skip = 1, col_types = "ccciddicciicccciiddcccd")
 proteinFasta <- readAAStringSet("openProtAllPredicted_38.fasta") 
 
 #Get IDs for subsets of interest: 2peptide and any evidence
-IDalt2peptide <- unique(tsvFile[which(tsvFile$`protein.type` == "AltProt" & (tsvFile$`MS.score` > 1)),]$`protein.accession.numbers`)
-IDaltEvidence <- unique(tsvFile[which(tsvFile$`protein.type` == "AltProt" & (tsvFile$`MS.score` != 0 | tsvFile$`TE.score` != 0)),]$`protein.accession.numbers`)
+#Also filters out some altProts that are isoforms on other transcripts and therefore unlikely to be true nORFs
 
-#Dataframes with DNA and AA seqs
-df <- data.frame(str_split(names(dnaFasta), "\\|", simplify = T)[,1], paste(dnaFasta))
-colnames(df) <- c("altIDs", "DNAseq")
+IDalt2peptide <- tsvFile %>% 
+  filter(`protein type` == "AltProt" & `MS score` > 1) %>% 
+  filter(!grepl('II', `protein accession (others)`)) %>% 
+  select(`protein accession numbers`) %>% 
+  distinct()
+IDaltEvidence <- tsvFile %>% 
+  filter(`protein type` == "AltProt" & (`MS score` != 0 | `TE score` != 0)) %>% 
+  filter(!grepl('II', `protein accession (others)`)) %>% 
+  select(`protein accession numbers`) %>% 
+  distinct() 
+
+
+#Dataframe with AA seq
 dfp <- data.frame(str_split(names(proteinFasta), "\\|", simplify = T)[,1], paste(proteinFasta))
 colnames(dfp) <- c("altIDs", "AAseq")
 
@@ -32,44 +40,36 @@ gtfFile <- tibble(
   seqname = tsvFile$chr,
   source = "OpenProt",
   feature = "altORF",
-  start = tsvFile$start.genomic.coordinates,
-  end = tsvFile$stop.genomic.coordinates,
+  start = tsvFile$`start genomic coordinates`,
+  end = tsvFile$`stop genomic coordinates`,
   score = ".",
   strand = tsvFile$strand,
-  frame = "0",
-  altIDs = tsvFile$protein.accession.numbers)
+  frame = ".",
+  altIDs = tsvFile$`protein accession numbers`) 
 
 if (twoPeptide) {
   #Create bed file
-  BEDalt2peptide <- bedFile[which(bedFile$X4 %in% IDalt2peptide),]
+  BEDalt2peptide <- bedFile[which(bedFile$X4 %in% IDalt2peptide$`protein accession numbers`),]
   write_tsv(BEDalt2peptide, path = "openProtAlt2peptide_38.bed", col_names = F)
   #Create gtfFile
   gtfFile <- gtfFile %>% 
-    filter(altIDs %in% IDalt2peptide) %>%
-    left_join(df, by = "altIDs" ) %>%
+    filter(altIDs %in% IDalt2peptide$`protein accession numbers`) %>%
     left_join(dfp, by = "altIDs") %>%
     distinct() %>%
-    mutate(attribute = paste0('gene_id "', altIDs,'"; transcript_id "', altIDs, '.X" transcript_sequence "', DNAseq,'"; AA_seq "', AAseq,'"' )) %>%
-    select(-altIDs, -DNAseq, -AAseq)
+    mutate(attribute = paste0('gene_id "', altIDs,'"; transcript_id "', altIDs, '.X"; AA_seq "', AAseq,'";' )) %>%
+    select(-altIDs, -AAseq)
   write.table(gtfFile, file = "openProtAlt2peptide_38.gtf", col.names = F, row.names = F, sep = "\t", quote = F)
   
 } else {
   #Create bed file
-  BEDaltEvidence <- bedFile[which(bedFile$X4 %in% IDaltEvidence),]
+  BEDaltEvidence <- bedFile[which(bedFile$X4 %in% IDaltEvidence$`protein accession numbers`),]
   write_tsv(BEDaltEvidence, path = "openProtAltEvidence_38.bed", col_names = F)
   #Create gtf file
   gtfFile <- gtfFile %>% 
-    filter(altIDs %in% IDaltEvidence) %>%
-    left_join(df, by = "altIDs" ) %>%
+    filter(altIDs %in% IDaltEvidence$`protein accession numbers`) %>%
     left_join(dfp, by = "altIDs") %>%
     distinct() %>%
-    mutate(attribute = paste0('gene_id "', altIDs,'"; transcript_id "', altIDs, '.X" transcript_sequence "', DNAseq,'"; AA_seq "', AAseq,'"' )) %>%
-    select(-altIDs, -DNAseq, -AAseq)
+    mutate(attribute = paste0('gene_id "', altIDs,'"; transcript_id "', altIDs, '.X"; AA_seq "', AAseq,'";' )) %>%
+    select(-altIDs, -AAseq)
   write.table(gtfFile, file = "openProtAltEvidence_38.gtf", col.names = F, row.names = F, sep = "\t", quote = F)
 }
-
-
-
-
-
-
