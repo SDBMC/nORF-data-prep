da79de1b20ab34cb78a34a5beff2800e86b26b99
Matt Neville <mattneville17@gmail.com>
2019-04-24 12:53:26 +0100

    Update processDatasets.R

--- processDatasets.R
+++ processDatasets.R
@@ -3,11 +3,11 @@ args = commandArgs(trailingOnly=TRUE)
 
 #Test for compatible argument. If not, return an error
 if (length(args) == 1) {
-  if (!(args[1] %in% c("openprot", "openprot2pep","sorfs", "sorfsMS", "all"))) {
-    stop("Invalid argument. Choose one of (openprot, openprot2pep, sorfs, sorfsMS, all).", call.=FALSE)
+  if (!(args[1] %in% c("openprotEnsembl", "openprotRefseq","sorfs", "sorfsMS", "all"))) {
+    stop("Invalid argument. Choose one of (openprotEnsembl, openprotRefseq, sorfs, sorfsMS, all).", call.=FALSE)
   } 
 } else {
-  stop("Invalid argument. Choose one of (openprot, openprot2pep, sorfs, sorfsMS, all).", call.=FALSE)
+  stop("Invalid argument. Choose one of (openprotEnsembl, openprotRefseq, sorfs, sorfsMS, all).", call.=FALSE)
 }
 source('processingFunctions.R')
 
@@ -16,12 +16,12 @@ library(Biostrings)
 
 #Function calls appropriate functions from 'processingFunctions.R' based on supplied argument
 processDataset <- function(dataset) {
-  if (dataset == "openprot") {
-    openprot <- processOpenProt()
+  if (dataset == "openprotEnsembl") {
+    openprot <- processOpenProt(annotation = "ensembl")
     return(openprot)
-  } else if (dataset == "openprot2pep") {
-    openprot2pep <- processOpenProt(dataset = "openprot2pep")
-    return(openprot2pep)
+  } else if (dataset == "openprotRefseq") {
+    openprotRefseq <- processOpenProt(annotation = "refseq")
+    return(openprotRefseq)
   } else if (dataset == "sorfs") {
     sorfs <- processSorfs()
     return(sorfs)
@@ -29,25 +29,22 @@ processDataset <- function(dataset) {
     sorfsMS <- processSorfs(dataset = "sorfsMS")
     return(sorfsMS)
   } else if (dataset == "all") {
-    openprot <- processOpenProt()
+    openprot <- processOpenProt(annotation = "ensembl")
     sorfs <- processSorfs()
     combined <- combineNovelORFs(sorfs,openprot)
     return(combined)
   }}
 
-novelORFtable <- processDataset(args[1])
+novelORFtableOriginal <- processDataset(args[1])
+
+novelORFtableNamed <- addIDs(novelORFtableOriginal)
 
 #Convert to bed12 and write out
-novelORFbed <- createBed12(novelORFtable)
+novelORFbed <- createBed12(novelORFtableNamed) 
 write_tsv(novelORFbed, path = paste0(args[1],"_38.bed"), col_names = F)
 
 #Convert to gtf and write out
-novelORFgtf <- createGTF(novelORFtable)
+novelORFgtf <- createGTF(novelORFtableNamed)
 write.table(novelORFgtf, paste0(args[1],"_38.gtf"), col.names = F, row.names = F, sep = "\t", quote = F)
-
-
-
-
-
 
 
